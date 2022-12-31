92e3af0de872367aad1a028d4afa2d271482d72c
Eliton Peroni <eliton_peroni@hotmail.com>
2022-12-31 17:51:30 -0300

    Other Entitites and Second Migration

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -12,8 +12,11 @@ namespace SalesWebMVC.Data
         public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
             : base(options)
         {
+
         }
 
-        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
